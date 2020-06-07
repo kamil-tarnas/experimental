a43723e6cb0911f51e07c60de0f8b49b4c1ba928
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-06-07 14:28:18 +0200

    Adding description to constexpr conclusions

--- constexpr.cpp
+++ constexpr.cpp
@@ -3,6 +3,11 @@ constexpr int Square(int value)
    return value * value;
 }
 
+// The main takeaway is that is you want do not have an easy way of knowing if the object would be a constexpr and put into data segment 
+// If you care about the binary size (to the extend of caring of the amount of data put into data segment of memory for constexpr objects"
+// the you have no control of that (in the sense that object can be implicitly constexpr and there is not keyword for prohibiting that - there is no 'non-constexpr')
+// But the con is, that even if you don't declare object as constexpr they could end up being consexpr if the requirements for creating such object are fulfilled. 
+
 int main()
 {
    // Probably will be initialized during compile time and put into data segment of memory even without the constexpr specifier
