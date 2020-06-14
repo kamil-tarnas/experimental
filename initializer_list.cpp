1b69dcafe5d96481772d3033592edcd35a58508c
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-06-14 15:59:44 +0200

    Adding std::initializer_list twiddling and example of associative container

---
+++ initializer_list.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+//?
+//
+//template<class T, class U>
+//struct is_same : std::false_type {};
+// 
+// template<class T>
+// struct is_same<T, T> : std::true_type {};
+
+using std::cout;
+using std::endl;
+
+template<typename T, typename U>
+class IsSame
+{
+   public:
+      IsSame(T, U);
+      void GetIsSame();
+   private:
+      bool isSame = false;
+};
+
+template<typename T, typename T>
+class IsSame
+{
+   public:
+      IsSame(T, T);
+   private:
+   bool isSame = true;
+};
+
+template<typename T, typename U>
+void IsSame::GetIsSame()
+{
+   cout << isSame << endl;
+}
+
+int main()
+{
+   // In different standards different type is deduced (check C++11, C++14 and so on?) Josuttis?
+   auto k{1}; // Deduced to be int in all standards?
+
+   auto i = {1};
+
+   // By template metaprograming and SFINE could get to know the type of j
+   decltype(k) j = k;
+   
+   // Instantiate the IsSame template class and create an object
+   IsSame isItReallyTheSameType(j, i);
+
+   cout << isItReallyTheSameType.GetIsSame() << endl;
+
+   auto list = {1, 3, 2, 4};
+   cout << list.size() << endl;
+   cout << j << endl;
+   return 0;
+}
--- special_operators.cpp
+++ special_operators.cpp
@@ -2,6 +2,7 @@
 #include <utility>
 #include <string>
 
+// Move template to .h file?
 template<typename T>
 class AssocStringContainer
 {
@@ -11,11 +12,50 @@ public:
 	~AssocStringContainer();
 
 	// Interface functions
+   T& operator[](std::string);
 
 private:
-	std::vector<std::pair<std::string, T>> storage;
+	std::vector<std::pair<std::string, T>> mStorage;
 
 };
+
+// Overload on && and & for the case if it is called with
+// string that is an r-value and we can steal the guts from
+// or it is called with an l-value that might/would be used
+// by the called of this function
+// But then you should be cautious about the usage of 
+// std::move in make_pair
+// Now is save as taken-by-value argument is owned by the 
+// function body and does not have any dependency with
+// the caller of operator[]
+template<typename T>
+T& AssocStringContainer::operator[](std::string string)
+{
+   for (const auto&: mStorage)
+   {
+      if (mStorage.first == string)
+      {
+         return mStorage.second;
+      }
+   }
+   // Do not create a new variable? Create r-value of {string, T}
+   // that will be directly fed to the push_back argument?
+   // Would that auto even deduce std::pair?
+   auto newElement = std::make_pair(std::move(string), 0); 
+   // If the element cannot be found - push the requested string 
+   // along with '0' value integer to the mStorage
+   // std::move? Locality principle? push_back versus emplace_back?
+   // Both are overloaded on r-value references &&
+   // Perfect forwarding and piecewise-construct?
+   // mStorage.push_back({string, 0});
+   mStorage.push_back(std::move(newElement)); // Will it be forwarded? Ctor is not needed to be called, so no?
+   // The destructor was not called is it save to return newElem?
+   // Dangling reference newElem will be returned
+   // Need to return the reference to the actual object in mStorage
+   return mStorage.back().second;
+   // The '0' needs then to be convertible to T
+   // How to make it a general neutral T type value?
+}
 
 int main()
 {
