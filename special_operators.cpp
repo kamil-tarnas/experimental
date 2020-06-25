5ead8b25d728037ddb2045024e3b86e344b4176d
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-06-25 16:31:47 +0200

    Template disambiguation for for_each and not comprehending the template deduction rules

--- special_operators.cpp
+++ special_operators.cpp
@@ -51,7 +51,7 @@ public:
    {
    }
 
-   void operator()(complex& c) const
+   void operator()(complex& c) const // Latest fail - this is the function mentioned!
    {
       // Add the held state value to the argument that operator() is called with
       c += value;
@@ -79,17 +79,20 @@ void for_each_complicated(ContainerType<ElementType>::iterator beginIter, // Thi
 
 */
 
-//The alternative
+// The alternative - actually calling it with the arguments which belong to std:: drags the namespace into
+// the area of template overloading because of ADL. Need to disambiguate. TODO: Other options besides introducing a namespace?
+namespace experimental
+{
 template<typename Iter, typename Callable>
 Callable for_each(Iter begin, Iter end, Callable action)
 {
    while (begin != end) 
    {
-      action(*begin++); 
+      action(*begin++); // Latest fail - cannot bind non-const lvalue reference of type ‘complex&’ {aka ‘std::complex<double>&’} to an rvalue of type ‘complex’ {aka ‘std::complex<double>’}
    }
    return action; 
 }
-
+}
 
 // Free function for adding to both std::vector and std::list
 // Make the parameters optional by passing a pointer to a vec and list?
@@ -103,8 +106,10 @@ void AddToVectorAndList(std::vector<T>& vector, std::list<T>& list, complex z)
    // addFunctor() creates a temporary, which is then copied as an argument to for_each?
    // If yes then it is very suboptimally done? Not really so, the argument will be then
    // used repeatedly by for_each (during loop execution)
-   for_each(vector.begin(), vector.end(), Add{2, 3}); // Constructor with initializer list is needed here?
-   for_each(list.begin(), list.end(), Add{z});
+   //
+   // That's ambiguous - the ambiguity is with std::for_each, probably because of ADL
+   experimental::for_each(vector.begin(), vector.end(), Add{2, 3}); // Constructor with initializer list is needed here?
+   experimental::for_each(list.begin(), list.end(), Add{z}); // Will that create a temporary? Would it be moved?
 }
 
 
@@ -201,7 +206,10 @@ int main()
    
    std::cout << mySubsequentInteger << std::endl;
 
-
+   std::vector<int> vec{1, 4, 5, 3, 6};
+   std::list<int> list{2, 3, 5, 3, 4};
+   complex z{1, 2};
+   AddToVectorAndList(vec, list, z);
 
 	return 0;
 }
