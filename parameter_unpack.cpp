7297673a2858771ed4e8122b9488ae4898e310ab
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-07-01 20:25:16 +0200

    Parameter pack twiddling

---
+++ parameter_unpack.cpp
@@ -0,0 +1,37 @@
+// https://stackoverflow.com/questions/1657883/variable-number-of-arguments-in-c
+
+#include <iostream>
+
+// The variable "firstArgument" should probably be 
+// named differently, as in the second call (the first recursion call)
+// it would be the first parameter from parameter pack (the first
+// argument from Args.., arguments).
+
+// If only one arument is left in the unpacking it will call the below
+// instantiation for T of function template
+template<typename T>
+void VariadicPrint(T baseArgument) 
+{
+   using namespace std;
+   cout << baseArgument << endl;
+}
+   
+template<typename T, typename... Args>
+void VariadicPrint(T firstArgument, Args... arguments)
+{
+   using namespace std;
+
+   cout << firstArgument << endl;
+   VariadicPrint(arguments...);
+}
+
+int main()
+{
+   int i = 5;
+   int j = 3;
+   double k = 5.2;
+
+   VariadicPrint(i, j, k);
+
+   return 0;
+}
---
+++ parameter_unpack_no_second_template_arg.cpp
@@ -0,0 +1,37 @@
+// https://stackoverflow.com/questions/1657883/variable-number-of-arguments-in-c
+
+#include <iostream>
+
+// The variable "firstArgument" should probably be 
+// named differently, as in the second call (the first recursion call)
+// it would be the first parameter from parameter pack (the first
+// argument from Args.., arguments).
+
+// If only one arument is left in the unpacking it will call the below
+// instantiation for T of function template
+template<typename T>
+void VariadicPrint(T baseArgument) 
+{
+   using namespace std;
+   cout << baseArgument << endl;
+}
+   
+template<typename... Args>
+void VariadicPrint(Args... arguments)
+{
+   using namespace std;
+
+   cout << arguments... << endl; // should << the argument? Can it be expanded like this?
+   VariadicPrint(arguments...);
+}
+
+int main()
+{
+   int i = 5;
+   int j = 3;
+   double k = 5.2;
+
+   VariadicPrint(i, j, k);
+
+   return 0;
+}
