ae0bd2f096ab2d14f5bf982dd7433fc57c7ee081
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-12-07 12:14:57 +0100

    Adding more border condition checks for overflow_calculator

--- overflow_calculator.cpp
+++ overflow_calculator.cpp
@@ -4,23 +4,23 @@
 
 struct timestamps
 {
-  int current;
-  int start;
-  int end;
+  unsigned current;
+  unsigned start;
+  unsigned end;
 };
 
 void isInRange()
 { 
    //Expected output is: 1 1 1 . 0 0 0 0 0
-   const int numOfTests = 8;
-   timestamps stamps[numOfTests] = {{4, 10239, 5}, {10239, 10238, 3}, {5, 1, 7}, {10, 10238, 3}, {10, 10239, 5}, {10, 1, 7}, {10, 11, 12}, {10, 10238, 3}}; //last should fail!
+   const int numOfTests = 10;
+   timestamps stamps[numOfTests] = {{4, 10239, 5}, {10239, 10238, 3}, {5, 1, 7}, {10, 10238, 3}, {10, 10239, 5}, {10, 1, 7}, {10, 11, 12}, {10, 10238, 3}, {10239, 10239, 5}, {5, 10238, 5}}; //last should fail!
    for (int i = 0; i < numOfTests; i++)
    {
       bool isInRange= !((stamps[i].current - stamps[i].start) <= 0 && (stamps[i].current - stamps[i].end) >= 0);
       bool isInRangeAlt = (!((stamps[i].current - stamps[i].start) <= 0) || !((stamps[i].current - stamps[i].end) >= 0)); 
       bool isInRangeOld = (stamps[i].current >= stamps[i].start && stamps[i].current <= stamps[i].end);
-      bool isInRangeExp = (stamps[i].start > stamps[i].end) ? !((int)(stamps[i].current - stamps[i].start) <= 0 && (int)(stamps[i].current - stamps[i].end) >= 0) : (stamps[i].current >= stamps[i].start && stamps[i].current <= stamps[i].end);
-      bool isInRangeExp2 = ((stamps[i].start - stamps[i].current >= 0) && !(stamps[i].end - stamps[i].current >=0));
+      bool isInRangeExp = (stamps[i].start > stamps[i].end) ? !(((int)(stamps[i].current - stamps[i].start) < 0) && ((int)(stamps[i].current - stamps[i].end) > 0)) : (stamps[i].current >= stamps[i].start && stamps[i].current <= stamps[i].end);
+      //bool isInRangeExp = (stamps[i].start > stamps[i].end) ? (!((int)(stamps[i].current - stamps[i].start)) > 0 || !((int)(stamps[i].current - stamps[i].end)) < 0) : (stamps[i].current >= stamps[i].start && stamps[i].current <= stamps[i].end);
       bool isInRangeExp3 = !(stamps[i].current - stamps[i].start <= 0) && (stamps[i].current - stamps[i].end <=0);
       bool isInRangeDlMacCe = !has_expired(stamps[i].start, stamps[i].current) && has_expired(stamps[i].end, stamps[i].current);
       std::cout << " NEW:" <<  isInRange;
