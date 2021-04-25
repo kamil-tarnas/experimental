f0a99ecd29b9fcc9985fbe142edfdbd1d6eba5be
kamil-tarnas <kamil.tarnas@gmail.com>
2021-04-25 14:44:44 +0200

    Update copy_to_master_repo.sh to be aligned with C++ files in log_stat

--- copy_to_master_repo.sh
+++ copy_to_master_repo.sh
@@ -3,3 +3,5 @@
 #copy_to_master_repo
 cp stat.sh ../log_stat_GitHub/log_stat/log_stat.sh
 cp issues ../log_stat_GitHub/log_stat/issues
+cp -R cpp ../log_stat_GitHub/log_stat/
+cp compile ../log_stat_GitHub/log_stat/compile
