a2de2446e1e1db9dcff10a3eeb36c13e2e6135c9
kamil-tarnas <kamil.tarnas@gmail.com>
2021-04-25 19:39:45 +0200

    Having a case for 'sponge' not installed

--- change_tabs_to_spaces.sh
+++ change_tabs_to_spaces.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-for i in $(find . -type f \( -name "*.cpp" -o -name "*.hpp" \)); do
-  expand -i -t 2 "$i" | sponge "$i"
-done
+
+isSpongeInstalled=$(which sponge)
+
+if [ -n "$isSpongeInstalled" ]
+then
+  for i in $(find . -type f \( -name "*.cpp" -o -name "*.hpp" \)); do
+    expand -i -t 2 "$i" | sponge "$i"
+  done
+else
+   echo "'sponge' from moreutils is not installed!"
+fi
